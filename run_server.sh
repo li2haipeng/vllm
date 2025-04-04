3fb190a2075a8d73c5b10471c63e31a19c1bbd3f
Haipeng <li2haipeng@gmail.com>
2025-04-04 00:56:06 +0000

    update server envs

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,11 @@
-export VLLM_USE_DEEPGEMM=1
+export VLLM_USE_DEEPGEMM=0
 export VLLM_WORKER_MULTIPROC_METHOD='spawn'
+export VLLM_ATTENTION_BACKEND="FLASH_ATTN"
+export VLLM_FLASH_ATTN_VERSION=3
+export VLLM_MLA_DISABLE=1
 # export VLLM_ATTENTION_BACKEND='FLASH_ATTN'
 python -m vllm.entrypoints.openai.api_server \
-    --model /home/ubuntu/models/Llama-3.3-70B-Instruct \
+    --model /home/ubuntu/models/DeepSeek-R1 \
     --tensor-parallel-size 8 \
     --max-model-len 8192 \
     --max-num-seqs 8 \
