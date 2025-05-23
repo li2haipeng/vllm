f0a6eaadde1129260daa83a8657a5f08ac28c06c
Haipeng Li <ubuntu@ip-172-31-13-247.us-east-2.compute.internal>
2025-05-23 17:55:57 +0000

    add run vllm

---
+++ run_vllm.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import time
+
+from vllm import LLM, SamplingParams
+
+os.environ["VLLM_TORCH_PROFILER_DIR"] = "./vllm_profile_simple_v8"
+# os.environ["VLLM_ENABLE_V1_MULTIPROCESSING"] = "0"
+prompts = [
+    "Hello, my name is Albert, and I am a scientist. I am working on a project to",
+]
+
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=5)
+if __name__ == "__main__":
+
+    # model_id = "/home/ubuntu/models/Llama-4-Maverick-17B-128E-Instruct-FP8/"
+    # model_id = "nm-testing/Mixtral-8x7B-Instruct-v0.1-FP8-quantized"
+    # model_id = "nm-testing/Mixtral-8x7B-Instruct-v0.1-FP8-Dynamic"
+    # model_id = "/home/ubuntu/models/DSR1"
+    # model_id = "/home/ubuntu/models/Llama-3.3-70B-Instruct-quantized.w4a16"
+    # model_id = "/home/ubuntu/models/Llama-4-Scout-per-channel-w4a16"
+    # model_id = "/home/ubuntu/models/Llama-4-Scout-per-group-w4a16"
+    model_id = "/home/ubuntu/models/Llama-4-Scout-17B-16E-Instruct-W4A8-routed_only"
+    llm = LLM(
+        model=model_id,
+        tensor_parallel_size=8,
+        max_model_len=32768,
+        enforce_eager=False,
+        compilation_config={"cudagraph_capture_sizes": [1,4,8], "compile_sizes": [1,4,8]},
+    )
+
+    llm.start_profile()
+    outputs = llm.generate(prompts, sampling_params)
+    llm.stop_profile()
+
+    print("-" * 50)
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}\nGenerated text: {generated_text!r}")
+        print("-" * 50)
